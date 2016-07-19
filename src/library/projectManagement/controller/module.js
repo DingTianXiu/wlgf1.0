99ed455d66a6d5979827a9e94a6466e44c6b0c4d
dingtianxiu <295434665@qq.com>
2016-07-19 14:54:19 +0800

    项目管理切图

--- src/library/projectManagement/controller/module.js
+++ src/library/projectManagement/controller/module.js
@@ -5,4 +5,11 @@
 angular.module("app.library.projectManagement.controller",[])
     .controller("projectManagementCtrl",[function () {
 
+        /*宽度随浏览器clientWidth高度改变而改变*/
+        var width = document.body.clientWidth;
+        document.getElementsByClassName("projectManagement")[0].style.width = (width-201)+"px";
+        window.onresize = function () {
+            width = document.body.scrollWidth;
+            document.getElementsByClassName("projectManagement")[0].style.width = (width-201)+"px";
+        };
     }]);
